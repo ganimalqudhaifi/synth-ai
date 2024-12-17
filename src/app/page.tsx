99e9896b3770dc6a360b542ce962dc8ea99d5b9f
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2024-12-17 21:47:29 +0700

    Create UI components for dark mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { DarkModeToggle } from "@/components/dark-mode-toggle";
+
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="relative container min-h-screen min-w-full flex justify-center items-center">
+      <DarkModeToggle />
+    </div>
+  );
 }
